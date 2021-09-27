713cf4d7fb2443e75c7589e8a3c885127f29f923
Zane Rockenbaugh <zane@liquid-labs.com>
2021-09-26 19:26:04 -0500

    hasn't been committed in awhile

--- data/orgs/settings.sh
+++ data/orgs/settings.sh
@@ -1,8 +1,9 @@
 ORG_COMMON_NAME='Liquid Labs'
 ORG_GITHUB_NAME='Liquid-Labs'
 ORG_LEGAL_NAME='Liquid Labs, LLC'
-ORG_ADDRESS='907 Isle of Man Ct
-Pflugerville, TX 78660'
+ORG_ENTITY_DESCRIPTION='a Texas limited liability company'
+ORG_ADDRESS='4419 Gillis Str, #B
+Austin, TX 78745'
 ORG_NAICS='1'
 ORG_NPM_SCOPE='liquid-labs'
 ORG_NPM_REGISTRY='https://registry.npmjs.org/'
---
+++ records/audits/active/20200515000334-code-change-zane/audit.sh
@@ -0,0 +1,5 @@
+START="20200515000334"
+DESCRIPTION="Change code audit starting 2020-05-15 00:03 UTC by zane@liquid-labs.com."
+DOMAIN="code"
+SCOPE="change"
+OWNER="zane@liquid-labs.com"
